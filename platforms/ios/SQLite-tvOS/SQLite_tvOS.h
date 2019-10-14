13bd44db2fa01f0b1eee207d4bc8dcec9a0f3484
Janani Ramesh <janani.ramesh@intl.att.com>
2019-10-14 12:02:01 +0530

    Added tvOs target

---
+++ platforms/ios/SQLite-tvOS/SQLite_tvOS.h
@@ -0,0 +1,13 @@
+//
+//  SQLite_tvOS.h
+//  SQLite-tvOS
+//
+//  Created by Janani Ramesh on 08/10/19.
+//  Copyright © 2019 Andrzej Porebski. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SQLite_tvOS : NSObject
+
+@end
---
+++ platforms/ios/SQLite-tvOS/SQLite_tvOS.m
@@ -0,0 +1,13 @@
+//
+//  SQLite_tvOS.m
+//  SQLite-tvOS
+//
+//  Created by Janani Ramesh on 08/10/19.
+//  Copyright © 2019 Andrzej Porebski. All rights reserved.
+//
+
+#import "SQLite_tvOS.h"
+
+@implementation SQLite_tvOS
+
+@end
